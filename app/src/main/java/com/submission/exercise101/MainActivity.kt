8b2f995bcf11b8995b626dec19fbf0cc020dee8d
Fariz5 <iqball.frz@gmail.com>
2022-05-07 16:10:36 +0700

    second commit

--- app/src/main/java/com/submission/exercise101/MainActivity.kt
+++ app/src/main/java/com/submission/exercise101/MainActivity.kt
@@ -8,5 +8,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         println("first change")
+        println("second change")
     }
 }
