caea0daa45c35f838547afc16175ed4f615f0dd1
nbee7 <daffadit0721@gmail.com>
2022-05-08 17:57:49 +0700

    seven feature

--- app/src/main/java/com/submission/exercise101/MainActivity.kt
+++ app/src/main/java/com/submission/exercise101/MainActivity.kt
@@ -7,9 +7,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("fifth feature")
-        println("lala")
-        println("thirds feature")
-        println("six feature")
     }
 }
