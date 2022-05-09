17f11bcf7dde1ea6f83ea69d6a3ef46385692831
Fariz5 <iqball.frz@gmail.com>
2022-05-09 10:00:44 +0700

    commit_master

--- app/src/main/java/com/submission/exercise101/MainActivity.kt
+++ app/src/main/java/com/submission/exercise101/MainActivity.kt
@@ -9,5 +9,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         println("fifth feature")
         println("third feature")
+        println("commit master")
     }
 }
